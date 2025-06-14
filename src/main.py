62c94846c12eef9d471cd8915200d31fade2585e
Juan Siagian <jsa.siagian@gmail.com>
2025-06-14 20:13:22 +0700

    [feat] measure initialization time

--- src/main.py
+++ src/main.py
@@ -5,22 +5,28 @@ from pathlib import Path
 import time
 
 if __name__ == '__main__':
+    startI = time.perf_counter()
+    SearchEngine.Initialize()
+    endI = time.perf_counter()
+    print(f"Init Time: {endI - startI:.4f}s")
+
     # Driver
-    type = input("Algorithm? (KMP / BM / AC): ")
-    max = int(input("Maximum files returned: "))
-    n = int(input("Number of keywords: "))
-    keywords = []
-    for i in range(n):
-        keywords.append(input(f"Keywords {i+1}: "))
-    print(SearchEngine.SearchExact(keywords, type, max))
+    # type = input("Algorithm? (KMP / BM / AC): ")
+    # max = int(input("Maximum files returned: "))
+    # n = int(input("Number of keywords: "))
+    # keywords = []
+    # for i in range(n):
+    #     keywords.append(input(f"Keywords {i+1}: "))
+    # print(SearchEngine.SearchExact(keywords, type, max))
 
     # Timing mechanism
-    # start = time.perf_counter()
-    # end = time.perf_counter()
     # print(f"Time: {end - start:.4f}s")
 
     # Some testcases...
     # TC 1 
-    # print(SearchEngine.SearchExact(["work", "skills", "using", "tech"], "BM", 5))
+    startP = time.perf_counter()
+    print(SearchEngine.SearchExact(["work"], "BM", 5))
+    endP = time.perf_counter()
+    print(f"Process Time : {endP - startP:.4f}s")
     # TC 2 
     # print(SearchEngine.SearchExact(["and", "the", "to", "are"], "BM", 10))
