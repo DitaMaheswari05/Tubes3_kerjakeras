7f8c32b5750fde92bc5dc67662eaae50fc8afc91
Juan Siagian <jsa.siagian@gmail.com>
2025-06-14 19:48:15 +0700

    [feat] testcases

--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@
 from SearchEngine import SearchEngine
 from FileParser import FileParser
 from pathlib import Path
+import time
 
 if __name__ == '__main__':
+    # Driver
     type = input("Algorithm? (KMP / BM / AC): ")
     max = int(input("Maximum files returned: "))
     n = int(input("Number of keywords: "))
@@ -11,3 +13,14 @@ if __name__ == '__main__':
     for i in range(n):
         keywords.append(input(f"Keywords {i+1}: "))
     print(SearchEngine.SearchExact(keywords, type, max))
+
+    # Timing mechanism
+    # start = time.perf_counter()
+    # end = time.perf_counter()
+    # print(f"Time: {end - start:.4f}s")
+
+    # Some testcases...
+    # TC 1 
+    # print(SearchEngine.SearchExact(["work", "skills", "using", "tech"], "BM", 5))
+    # TC 2 
+    # print(SearchEngine.SearchExact(["and", "the", "to", "are"], "BM", 10))
